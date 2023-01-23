2fefd20a822c8c6d9d56cab949e8b8b387b0ab83
Camiloarceg <camiloarceg@gmail.com>
2023-01-23 17:29:14 -0500

    works just need to add exit

--- pass_gen.py
+++ pass_gen.py
@@ -8,6 +8,7 @@ import random
 import string
 import csv
 import base64
+import os
 
 
 def add_password(app_name, password, path):
@@ -15,33 +16,33 @@ def add_password(app_name, password, path):
 
     # encode the password
     encoded_password = base64.b64encode(password.encode("utf-8"))
-    print(f"encoded_password : {encoded_password}")
-    #row creation
+    # row creation
     row = [app_name.lower(), encoded_password]
     # add the encoded password to csv file
-    with open(path, 'a') as csv_file:
+    with open(path, "a") as csv_file:
         writer_object = csv.writer(csv_file)
         writer_object.writerow(row)
 
+
 def overwrite_password(path, app_name, new_password):
-    """ Overwrite the password """
+    """Overwrite the password"""
     target = 0
     rows = []
-    with open(path, 'r') as csv_file:
+    with open(path, "r") as csv_file:
         csv_reader = csv.reader(csv_file)
         for row in csv_reader:
             if row[0] == app_name:
                 continue
             rows.append(row)
-    print(rows)
-    with open(path, 'w') as csv_file:
+    with open(path, "w") as csv_file:
         csv_writer = csv.writer(csv_file)
         csv_writer.writerows(rows)
     add_password(app_name, new_password, path)
 
+
 def get_password(path, app_name):
-    """ Get a  decoded password """
-    with open(path, 'r') as csv_file:
+    """Get a  decoded password"""
+    with open(path, "r") as csv_file:
         # creating a csv reader object
         csv_reader = csv.reader(csv_file)
         # extracting field names through first row
@@ -51,10 +52,13 @@ def get_password(path, app_name):
             # found the matching app
             if app_name.lower() == row[0]:
                 # return the decoded password
-                return base64.urlsafe_b64decode(row[1]).decode("utf-8")
+                encoded_password = row[1].replace("'", "").replace("b", "")
+                decoded_password = base64.b64decode(encoded_password.strip()).decode("utf-8")
+                return decoded_password
+
 
 def has_data_rows(path):
-    """ check if the file has data rows """
+    """check if the file has data rows"""
     with open(path) as file:
         return file.readline() and file.readline()
 
@@ -63,11 +67,10 @@ def get_apps(path):
     """Return list of all apps in csv file."""
     apps = []
     # get data frame from file
-    with open(path, 'r') as csv_file:
+    with open(path, "r") as csv_file:
         csv_reader = csv.reader(csv_file)
         next(csv_reader)
         for row in csv_reader:
-            print(row)
             apps.append(row[0])
     return apps
 
@@ -85,7 +88,7 @@ def character_assignation():
                 print("Please enter a valid amount of characters")
                 continue
             break
-        except :
+        except:
             print("Please enter only valid type of characters")
     return char_num
 
@@ -120,32 +123,39 @@ if __name__ == "__main__":
     apps = []
     path = "passwords.csv"
 
+    if not os.path.exists(path):
+        with open(path, 'w') as csv_file:
+            # creating a csv writer object
+            csv_writer = csv.writer(csv_file)
+            # writing the fields
+            csv_writer.writerow(fields)
     while True:
         # app name request
-        app_name = input("Please enter your app name: ")
-        print(has_data_rows(path))
+        app_name = input("Please enter your app name: ").lower()
         # validate if file has data in it
         if has_data_rows(path):
             app_list = get_apps(path)
-            print(app_list)
             # check if app is already created
             if app_list.count(app_name) == 0:
                 char_num = character_assignation()
                 password = generate_password(char_num)
-                print(password)
                 add_password(app_name, password, path)
             # if app is already take a decision
             else:
-                decision = input("Seems you already have a password, 1 for view password or 2 for overwrite password: ")
-                if decision == '1':
+                decision = input(
+                    "Seems you already have a password, 1 for view password or 2 for overwrite password: "
+                )
+                if decision == "1":
                     # print the retieved password
-                    print(f"Your password for {app_name} is {get_password(path, app_name)}")
+                    print(
+                        f"Your password for {app_name} is {get_password(path, app_name)}"
+                    )
                 else:
                     # overwrite the password
                     char_num = character_assignation()
                     new_password = generate_password(char_num)
                     overwrite_password(path, app_name, new_password)
-        #if the file is empty or only with header fields
+        # if the file is empty or only with header fields
         else:
             char_num = character_assignation()
             password = generate_password(char_num)
