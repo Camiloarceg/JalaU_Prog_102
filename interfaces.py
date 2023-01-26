88aaa39c42c1c96f031d4a082737c215a3397211
Camiloarceg <camiloarceg@gmail.com>
2023-01-25 19:59:29 -0500

    improved version

--- interfaces.py
+++ interfaces.py
@@ -3,19 +3,35 @@ import optparse
 
 opts = optparse.OptionParser()
 
-opts.add_option("-f", "--file", dest='fname', help="This is the file name that you would like to read")
-opts.add_option("-i", "--interface", dest='interface', help="This is the interface that you would like to search")
+opts.add_option(
+    "-f",
+    "--file",
+    dest="fname",
+    help="This is the file name that you would like to read",
+)
+opts.add_option(
+    "-i",
+    "--interface",
+    dest="interface",
+    help="This is the interface that you would like to search",
+)
 
 (options, arguments) = opts.parse_args()
 
 with open(str(options.fname), "r") as f:
     lines = f.readlines()
     index = ""
+    # locate the interface an extrac index
     for line in lines:
         if options.interface in line:
             index = list(line)[21]
             break
-
+    # count 11 points and then validate if next char is index
     for line in lines:
-        if list(line)[21] is index:
-            print(line.strip())
+        point_count = 0
+        for char in line:
+            if point_count == 11:
+                if char is index:
+                    print(line.strip())
+            if char is ".":
+                point_count += 1
